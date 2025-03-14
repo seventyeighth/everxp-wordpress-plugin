b124d29f22ad7c955cda743305e652f3d8cb4159
1178 <amit@accessily.com>
2025-03-14 07:23:45 +0200

    Update everxp-plugin.php

--- everxp-plugin.php
+++ everxp-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: EverXP API Plugin
 Description: Provides API integration with shortcodes, Elementor widgets, and database sync.
-Version: 1.6.1
+Version: 1.6.2
 Author: Accessily LTD
 */
 
